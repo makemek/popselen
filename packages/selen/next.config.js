9ae1592a12e6e09448a274b1289a490a334e529c
Apipol Niyomsak <makemek101@hotmail.com>
2021-09-24 22:49:42 +0700

    poc

--- packages/selen/next.config.js
+++ packages/selen/next.config.js
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfig = {
+  webpack: (config, options) => {
+    return config;
+  },
+  eslint: {
+    // ESLint managed on the workspace level
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true,
+  },
+};
+
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+    ],
+  ],
+  nextConfig
+);
+
+module.exports = config;
