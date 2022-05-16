f46e93412a63b9f2aa487b02bc951d84e32aed2c
Apipol Niyomsak <makemek101@hotmail.com>
2022-05-16 21:57:39 +0700

    add recaptcha guard to /pop

--- packages/api/src/pop/pop.controller.ts
+++ packages/api/src/pop/pop.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { IpCountry } from './ipCountry.decorator';
 import { PopDto } from './pop.dto';
 import { PopService } from './pop.service';
+import { reCaptchaGuard } from './recaptcha.guard';
 
 @Controller('pop')
 export class PopController {
   constructor(private readonly popService: PopService) {}
 
   @Post()
+  @UseGuards(reCaptchaGuard('pop'))
   async pop(@Body() popDto: PopDto, @IpCountry() country: string) {
     await this.popService.add(country, popDto.count);
   }
