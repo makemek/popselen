0c0e461b145d1acf7a97bb6c7d89ace7513f7425
Apipol Niyomsak <makemek101@hotmail.com>
2022-05-19 21:29:06 +0700

    allow cross-origin * on non-production env

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,12 +3,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // Running recaptcha locally for development.
-    // Please check repo wiki for more details.
-    cors: !!process.env.LOCAL_RECAPTCHA,
-  });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  if (process.env.NODE_ENV !== "production") {
+    app.enableCors();
+  }
   await app.listen(+process.env.PORT || 3300);
 }
 bootstrap();
